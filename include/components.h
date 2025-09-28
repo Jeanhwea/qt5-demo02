7f4f210913a582a31a119e33167a32a2df84a57b
Jinghui Hu <hujinghui@buaa.edu.cn>
2025-09-28 18:11:42 +0800

    refactor(ui): 添加新的UI组件头文件引用
    添加JxHBoxLayout和JxWidget头文件引用以支持更多UI组件功能

--- include/components.h
+++ include/components.h
@@ -2,7 +2,9 @@
 #define _COMPONENTS_H_
 
 #include "ui/MainWindow.h"
+#include "ui/jx/JxHBoxLayout.h"
 #include "ui/jx/JxPushButton.h"
 #include "ui/jx/JxVBoxLayout.h"
+#include "ui/jx/JxWidget.h"
 
 #endif /* End of _COMPONENTS_H_ */
