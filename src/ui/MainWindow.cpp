557c04a05c878dbab048f302e580fd8ce1843fb8
Jinghui Hu <hujinghui@buaa.edu.cn>
2025-09-28 20:29:00 +0800

    refactor(ui): 优化主窗口布局并增加新按钮
    - 重命名 QApplication 变量名以提高可读性
    - 为主窗口设置零边距以消除默认间距
    - 添加第二个按钮并确保布局正确设置

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -9,6 +9,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     // 初始化主窗口
     this->setWindowTitle("DEMO");
     this->setGeometry(100, 100, 800, 600);
+    this->setContentsMargins(0, 0, 0, 0);
 
     // 初始化UI组件
     this->initUI();
@@ -24,15 +25,22 @@ void MainWindow::initUI()
 {
     widget = new JxWidget(this);
     JxVBoxLayout *layout = new JxVBoxLayout();
+    // layout->setContentsMargins(100, 100, 100, 100);
 
     JxPushButton *btn01 = new JxPushButton(widget);
     btn01->setText("Click Me");
     btn01->connect(btn01, &JxPushButton::clicked, this, &MainWindow::onBtnClick);
     layout->addWidget(btn01);
 
+    JxPushButton *btn02 = new JxPushButton(widget);
+    btn02->setText("Click Me 2");
+    // btn02->connect(btn02, &JxPushButton::clicked, this, &MainWindow::onBtnClick);
+    layout->addWidget(btn02);
+
     layout->addStretch();
 
     this->widget = widget;
+    widget->setLayout(layout);
     this->layout = layout;
     this->setCentralWidget(this->widget);
 }
