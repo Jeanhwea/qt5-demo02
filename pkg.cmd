53831453af5c3c7403177bb44ce40454780d81cf
Jinghui Hu <hujinghui@buaa.edu.cn>
2025-09-28 13:26:02 +0800

    update: 修改打包文件

--- pkg.cmd
+++ pkg.cmd
@@ -1,1 +1,7 @@
-cmake --build build --config Debug --target ALL_BUILD
+@echo off
+setlocal enabledelayedexpansion
+
+@REM set "CONFIG=Debug"
+set "CONFIG=Release"
+
+cmake --build build --config %CONFIG% --target ALL_BUILD
