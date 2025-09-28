4b9fa114507cd3bd4353da70ba2c776cfd26b9c1
Jinghui Hu <hujinghui@buaa.edu.cn>
2025-09-28 13:02:08 +0800

    style: 调整代码格式和clang-format配置
    修改MainWindow构造函数和析构函数的格式，使其更清晰易读
    更新.clang-format配置，禁止短函数单行显示

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -1,5 +1,9 @@
 #include "ui/MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {}
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
+{
+}
 
-MainWindow::~MainWindow() {}
+MainWindow::~MainWindow()
+{
+}
