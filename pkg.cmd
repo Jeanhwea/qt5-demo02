e245b8a771071985d1b608de1cd4152e8af21cc4
Jinghui Hu <hujinghui@buaa.edu.cn>
2025-09-28 13:32:34 +0800

    fix: 将默认构建配置改为Debug并移除WIN32_EXECUTABLE条件
    修改pkg.cmd将默认构建配置从Release改为Debug，便于开发调试
    移除CMakeLists.txt中WIN32_EXECUTABLE的条件判断，确保Windows平台始终生成GUI程序

--- pkg.cmd
+++ pkg.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
-@REM set "CONFIG=Debug"
-set "CONFIG=Release"
+set "CONFIG=Debug"
+rem set "CONFIG=Release"
 
 cmake --build build --config %CONFIG% --target ALL_BUILD
