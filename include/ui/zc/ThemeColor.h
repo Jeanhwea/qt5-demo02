03179919cda2c822567d54eb2c701202eb204ca2
Jinghui Hu <hujinghui@buaa.edu.cn>
2025-09-29 15:38:47 +0800

    refactor(ui): 更新主题颜色命名并优化按钮样式
    - 将ThemeColor中的颜色常量从BLUE_XXX重命名为PRIMARY_COLOR_XXX以提升语义化
    - 修改PrimaryButton的QSS样式，使用主题颜色并添加边框和圆角

--- include/ui/zc/ThemeColor.h
+++ include/ui/zc/ThemeColor.h
@@ -4,16 +4,16 @@
 #include <QString>
 
 namespace ThemeColor {
-const QString BLUE_050 = "#E3F2FD";
-const QString BLUE_100 = "#BBDEFB";
-const QString BLUE_200 = "#90CAF9";
-const QString BLUE_300 = "#64B5F6";
-const QString BLUE_400 = "#42A5F5";
-const QString BLUE_500 = "#2196F3";
-const QString BLUE_600 = "#1E88E5";
-const QString BLUE_700 = "#1976D2";
-const QString BLUE_800 = "#1565C0";
-const QString BLUE_900 = "#0D47A1";
+const QString PRIMARY_COLOR_050 = "#E3F2FD";
+const QString PRIMARY_COLOR_100 = "#BBDEFB";
+const QString PRIMARY_COLOR_200 = "#90CAF9";
+const QString PRIMARY_COLOR_300 = "#64B5F6";
+const QString PRIMARY_COLOR_400 = "#42A5F5";
+const QString PRIMARY_COLOR_500 = "#2196F3";
+const QString PRIMARY_COLOR_600 = "#1E88E5";
+const QString PRIMARY_COLOR_700 = "#1976D2";
+const QString PRIMARY_COLOR_800 = "#1565C0";
+const QString PRIMARY_COLOR_900 = "#0D47A1";
 }; // namespace ThemeColor
 
 #endif /* End of _THEMECOLOR_H_ */
--- source/ui/zc/PrimaryButton.cpp
+++ source/ui/zc/PrimaryButton.cpp
@@ -1,10 +1,18 @@
 #include "ui/zc/PrimaryButton.h"
 
-const QString PrimaryButton::QSS = R"(
+#include "ui/zc/ThemeColor.h"
+
+const QString PrimaryButton::QSS =
+    QString(R"(
 PrimaryButton {
-  background-color: none;
-}
-)";
+  color: %1;
+  background-color: %2;
+  height: 30px;
+  border-radius: 4px;
+  border: 1px solid %3;
+})")
+        .arg(ThemeColor::PRIMARY_COLOR_800, ThemeColor::PRIMARY_COLOR_100,
+             ThemeColor::PRIMARY_COLOR_800);
 
 PrimaryButton::PrimaryButton(JxWidget *parent) : JxPushButton(parent) {
   initUI();
