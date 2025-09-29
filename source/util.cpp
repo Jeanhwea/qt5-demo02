37c340809e5e9f124d0b2c27158066ef87f90230
Jinghui Hu <hujinghui@buaa.edu.cn>
2025-09-29 11:17:24 +0800

    perf(util): 使用QDateTime优化时间戳获取性能
    替换标准库chrono为Qt的QDateTime实现，简化代码并提高获取时间戳的效率

--- source/util.cpp
+++ source/util.cpp
@@ -1,13 +1,7 @@
 #include "util.h"
 
-#include <QDebug>
-#include <chrono>
+#include <qdatetime.h>
 
 long long Util::getCurrentTimestamp() {
-  auto now = std::chrono::system_clock::now();
-
-  auto duration = now.time_since_epoch();
-  auto seconds = std::chrono::duration_cast<std::chrono::seconds>(duration);
-
-  return seconds.count();
+  return QDateTime::currentDateTime().toMSecsSinceEpoch();
 }
