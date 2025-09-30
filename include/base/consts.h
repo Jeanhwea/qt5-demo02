5d309d2c4f05f760eb00ac73b4cbf63bd4453bea
Jinghui Hu <hujinghui@buaa.edu.cn>
2025-09-30 09:18:59 +0800

    refactor(base): 将常量从QString改为constexpr char*
    提高性能并减少内存使用，使用constexpr替代静态QString常量

--- include/base/consts.h
+++ include/base/consts.h
@@ -4,18 +4,18 @@
 #include <QString>
 
 namespace colors {
-static const QString WHITE = "\033[37m";
-static const QString GREEN = "\033[32m";
-static const QString YELLOW = "\033[33m";
-static const QString RED = "\033[31m";
-static const QString BRIGHT_RED = "\033[1;31m";
-static const QString DEFAULT = "\033[0m";
+inline constexpr const char *WHITE = "\033[37m";
+inline constexpr const char *GREEN = "\033[32m";
+inline constexpr const char *YELLOW = "\033[33m";
+inline constexpr const char *RED = "\033[31m";
+inline constexpr const char *BRIGHT_RED = "\033[1;31m";
+inline constexpr const char *DEFAULT = "\033[0m";
 }; // namespace colors
 
 namespace errcode {
-static const int SUCCESS = 0;
-static const int INIT_ERROR = 1;
-static const int PANIC = 2;
+inline constexpr int SUCCESS = 0;
+inline constexpr int INIT_ERROR = 1;
+inline constexpr int PANIC = 2;
 }; // namespace errcode
 
 #endif /* End of _CONSTS_H_ */
